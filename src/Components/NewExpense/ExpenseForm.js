ca84dd72813b7be783833b7c561ad967bc59ad04
gautam kumar <gautam.kumar@dhwaniris.com>
2023-09-24 13:06:40 +0530

    Expense tracker project created my first react project

---
+++ src/Components/NewExpense/ExpenseForm.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [titleEntered, setEnteredTitle] = useState("");
+  const [amountEntered, setEnteredAmount] = useState("");
+  const [dateEntered, setEnteredDate] = useState("");
+
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+
+    // not recomended....
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    //React will gurantee it is always be the latest snapshot keepind all schedule state in mind.
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+
+    //React will gurantee it is always be the latest snapshot keepind all schedule state in mind.
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+
+    //React will gurantee it is always be the latest snapshot keepind all schedule state in mind.
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: titleEntered,
+      amount: amountEntered,
+      date: new Date(dateEntered),
+    };
+
+    //Communicate up into child to parent...
+    props.onSaveExpenseData(expenseData);
+
+    setEnteredAmount("");
+    setEnteredTitle("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={titleEntered}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={amountEntered}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2023-01-01"
+            max="2025-01-01"
+            value={dateEntered}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
